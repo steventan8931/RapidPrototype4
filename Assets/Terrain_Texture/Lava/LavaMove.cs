0fdc6118e2921e6edd9c4c5db6f9c187d3b0153b
YueBao424 <88068787+YueBao424@users.noreply.github.com>
2021-09-13 11:05:36 +0800

    fix lava

--- Assets/Terrain_Texture/Lava/LavaMove.cs
+++ Assets/Terrain_Texture/Lava/LavaMove.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LavaMove : MonoBehaviour
 {
     public float scrollSpeed = 0.1f;
     Renderer rend;
