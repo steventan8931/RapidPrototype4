18fff5dc23e069cf18400bdd73ea5c20c692bc1f
Steven Tan <tan2001steven@hotmail.com>
2021-09-13 20:00:30 +1200

    MainMenu Added

---
+++ Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    public Quaternion m_StartRotation;
+    public float m_Rotation = -80;
+    public float m_RotationSpeed = 2.0f;
+
+    private void Start()
+    {
+        m_Rotation = -80;
+    }
+    public void Update()
+    {
+        m_Rotation -= m_RotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0.0f, m_Rotation, 0.0f);
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadEnd()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
