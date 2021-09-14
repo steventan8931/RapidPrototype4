07085d78cb72d99b469542f24489eee0651c1555
peter <hguanqi7@gmail.com>
2021-09-15 06:27:07 +0800

    Updated Buddy Scr

---
+++ Assets/BuddyScr.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuddyScr : MonoBehaviour
+{
+    public float CurrHp = 200;
+    public float MaxHp = 200;
+    public GameObject warningUi;
+    // Start is called before the first frame update
+    public void receiveDmg(float dmg)
+    {
+        CurrHp -= dmg;
+        //Show warning UI
+        warningUi.SetActive(true);
+        Invoke(nameof(disableWarning), 1.2f);
+        if(CurrHp <= 0)
+        {
+            CurrHp = 0;
+            // game over func
+        }
+    }
+
+    public void disableWarning()
+    {
+        warningUi.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
