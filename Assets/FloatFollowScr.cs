2a2f5500b09f1a105dec6b2ecce6b0e518ba4354
peter <hguanqi7@gmail.com>
2021-09-16 10:25:30 +0800

    Added Buddy hp bar

---
+++ Assets/FloatFollowScr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatFollowScr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform lookat;
+    public Vector3 offset;
+
+    private Camera cam;
+
+    public void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = cam.WorldToScreenPoint(lookat.position + offset);
+        if(transform.position != pos)
+        {
+            transform.position = pos;
+        }
+
+    }
+}
---
+++ Assets/Scripts/BuddyBarScr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BuddyBarScr : MonoBehaviour
+{
+    public BuddyScr buddy;
+    public Image buddyHpbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float percentage = buddy.CurrHp / buddy.MaxHp;
+        buddyHpbar.fillAmount = percentage;
+    }
+}
