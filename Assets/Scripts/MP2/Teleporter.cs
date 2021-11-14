5e2a8138450929ab96c8fbe9e6074307eb975931
Steven Tan <tan2001steven@hotmail.com>
2021-11-14 15:05:37 +1300

    Start on new teleporter script

--- Assets/Scripts/MP2/Teleporter.cs
+++ Assets/Scripts/MP2/Teleporter.cs
@@ -7,6 +7,9 @@ public class Teleporter : MonoBehaviour
     private FPCharacterMotor m_Motor;
     public Transform[] m_TeleportLocations;
 
+
+    public GameObject m_ShadowPrefab;
+
     private void Start()
     {
         m_Motor = FindObjectOfType<FPCharacterMotor>();
@@ -30,5 +33,49 @@ public class Teleporter : MonoBehaviour
         m_Motor.m_Controller.enabled = false;
         transform.position = _TeleportEnd.position;
         m_Motor.m_Controller.enabled = true;
+    }
+
+    private void MoveObjectToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            m_ShadowPrefab.transform.position = hitInfo.point;
+            m_ShadowPrefab.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+
+        //If object has a collider
+        if (hitInfo.collider != null)
+        {
+            //if (!hitInfo.collider.gameObject.CompareTag("Floor") && !hitInfo.collider.gameObject.CompareTag("Enemy"))
+            if (hitInfo.collider.gameObject.CompareTag("Placeable"))
+            {
+                //m_CurrentPlaceableObject.transform.GetChild(0).GetComponent<Renderer>().material = m_CanPlaceMat;
+                m_ShadowPrefab.GetComponent<PlaceableObject>().CheckValid(true);
+
+                BoxCollider PlaceableCollider = m_ShadowPrefab.gameObject.GetComponent<BoxCollider>();
+                PlaceableCollider.isTrigger = true;
+                Vector3 BoxCenter = m_ShadowPrefab.gameObject.transform.position + PlaceableCollider.center;
+                Vector3 HalfExtents = PlaceableCollider.size / 2;
+
+                if (Physics.CheckBox(BoxCenter, HalfExtents, Quaternion.identity))
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        //PlaceableCollider.isTrigger = false;
+                        m_ShadowPrefab.layer = 30;
+    
+                        m_ShadowPrefab = null;
+                    }
+                }
+            }
+            else
+            {
+                //m_CurrentPlaceableObject.transform.GetChild(0).GetComponent<Renderer>().material = m_CantPlaceMat;
+                m_ShadowPrefab.GetComponent<PlaceableObject>().CheckValid(false);
+            }
+        }
     }
 }
