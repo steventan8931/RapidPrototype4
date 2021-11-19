ca4c261ed0aca3db02bbfaf41d3edc8cb5f965bd
Steven Tan <tan2001steven@hotmail.com>
2021-11-19 20:15:13 +1300

    Win SCreen

---
+++ Assets/Scripts/MP2/RestrictControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestrictControl : MonoBehaviour
+{
+   public ShootingScr m_Shooter;
+   public FPCharacterMotor m_Motor;
+   public Teleporter m_Teleporter;
+   public CamSwitcher m_Cam;
+    public FpCameraScr m_FPCam;
+
+    private void Start()
+    {
+        m_Shooter = FindObjectOfType<ShootingScr>();
+        m_Motor = FindObjectOfType<FPCharacterMotor>();
+        m_Teleporter = FindObjectOfType<Teleporter>();
+        m_Cam = FindObjectOfType<CamSwitcher>();
+        m_FPCam = FindObjectOfType<FpCameraScr>();
+    }
+
+    public void DisableControls()
+    {
+        m_Shooter.enabled = false;
+        m_Motor.enabled = false;
+        m_Teleporter.enabled = false;
+        m_Cam.enabled = false;
+        m_FPCam.enabled = false;
+    }
+}
