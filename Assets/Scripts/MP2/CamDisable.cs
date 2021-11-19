a7dba0e4c320858d5ae98bae3d0367a940488d6e
Steven Tan <tan2001steven@hotmail.com>
2021-11-19 18:30:36 +1300

    Added Cut Scene at the start of the game

---
+++ Assets/Scripts/MP2/CamDisable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamDisable : MonoBehaviour
+{
+    public float m_Timer = 0.0f;
+    public float m_EndTimer = 0.0f;
+    public GameObject m_SetCamActive;
+    private void LateUpdate()
+    {
+        m_Timer += Time.deltaTime;
+        if (m_Timer > m_EndTimer)
+        {
+            if (m_SetCamActive)
+            {
+                m_SetCamActive.SetActive(true);
+            }
+            gameObject.SetActive(false);           
+        }
+    }
+}
