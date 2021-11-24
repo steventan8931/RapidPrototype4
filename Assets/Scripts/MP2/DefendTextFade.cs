1b5f3efd3c3eb30c19fa6e0bc93713a6b6dabda3
peter <hguanqi7@gmail.com>
2021-11-24 18:06:55 +0800

    added defend castle text

---
+++ Assets/Scripts/MP2/DefendTextFade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefendTextFade : MonoBehaviour
+{
+    public Animator fadeAnim;
+    public CanvasGroup canvasGroup;
+    private void Awake()
+    {
+        fadeAnim = GetComponent<Animator>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        Invoke(nameof(startFading), 4.5f);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void startFading()
+    {
+        fadeAnim.SetBool("IsFade", true);
+    }
+}
